4bc97a5d2a61e2b0094e40ba43b564c939e435c2
Kasilingam M <138505237+lingaa005@users.noreply.github.com>
2025-07-26 07:46:52 +0530

    Update settings.py 222

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jp#tex*s$ek!&r3*vi#eh@139-szu3)o4*gt_2rag1v$n33qj7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
